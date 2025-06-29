496c3c41f90e79b09ff04ba96fef8d378fe66fb7
patrick <bozhenglu@gmail.com>
2025-06-30 02:19:44 +0800

    第一個版本

---
+++ int.py
@@ -0,0 +1,31 @@
+#string
+"""
+print("Hello\n Mr.White")
+print("Hello\" Mr.White")
+
+"""
+
+#str相連
+print("Hello" + "Mr.White")
+#變數  函式 function
+"""
+print(phrase.lower())
+print(phrase.upper())
+#isupper  islower
+print(phrase.lower().islower())
+"""
+phrase = "Hello Mr.White"
+print(phrase[0])
+print(phrase.index("h"))
+
+print(phrase.replace("Mr.White","Kitty"))
+
+
+
+
+
+
+
+
+
+
---
+++ number.py
@@ -0,0 +1,21 @@
+from math import*
+
+
+number=8
+#轉換資料型態轉換資料型態
+#取絕對值
+print(abs(number))
+#資料型別轉換
+print(str(number))
+#次方
+print(pow(3,2))
+#告訴使用者最大(max) (min)
+print(max(2,4,33,44))
+#取四捨五入
+print(round(4.4))
+
+
+
+
+
+
